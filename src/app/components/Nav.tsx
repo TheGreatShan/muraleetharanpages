a730d360258bc5375a1cbfd410fb2a411efa9114
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 14:47:47 +0200

    Navbar has name as title

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+    return (
+        <div className={"bg-[#e7e5e4] text-black font-libre p-10"}>
+            <h1 className={"text-2xl"}>Shansai</h1>
+            <h1 className={"text-3xl"}>Muraleetharan</h1>
+        </div>
+    )
+}
--- src/app/components/design.tsx
+++ src/app/components/design.tsx
@@ -1,7 +1,15 @@
+import Nav from "muralee/app/components/Nav";
+
 export default function Design({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <div className={"bg-[#e7e5e4] h-screen text-black font-libre border-solid border-[7px] p-10 border-black"}>
-            {children}
-        </div>
+        <>
+            <div className={"border-solid border-[7px] border-black bg-[#e7e5e4] text-black font-libre"}>
+                <Nav/>
+
+                <div className={"h-screen p-10"}>
+                    {children}
+                </div>
+            </div>
+        </>
     )
 }
