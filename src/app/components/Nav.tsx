ac2a3b338913bfa56d575985ac14604aaf9a02a0
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 18:23:31 +0200

    navbar adapted to mobile

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Nav() {
     return (
         <div className={"bg-[#e7e5e4] text-black font-libre p-10"}>
-            <div className={"fixed left-0 ml-[100px]"}>
-                <h1 className={"text-2xl"}>Shansai</h1>
-                <h1 className={"text-3xl"}>Muraleetharan</h1>
+            <div className={"fixed left-0 md:ml-[100px] ml-[50px]"}>
+                <h1 className={"md:text-2xl text-sm"}>Shansai</h1>
+                <h1 className={"md:text-3xl text-sm"}>Muraleetharan</h1>
             </div>
             <div className={"fixed right-0 mx-10"}>
                 <Link href={"/"} title={"Home"} className={"mx-10"}>Home</Link>
