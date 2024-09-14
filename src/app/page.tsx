a856c74dad05c8bcac54d1c85e13d85ade46bb15
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 18:41:47 +0200

    border has gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,20 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-
-        <div
-            className="content-center bg-[#e7e5e4] h-screen text-black font-libre border-solid border-[7px] p-10 border-black">
-            <h1 className="md:text-6xl text-3xl">Shansai</h1>
-            <h1 className="md:text-8xl text-4xl">Muraleetharan</h1>
-            <div className={"mt-4"}>
-                <SocialIcon network={"linkedin"} url={"https://www.linkedin.com/in/shansai-muraleetharan"}
-                            bgColor={"black"} className={"mr-2"}/>
-                <SocialIcon network={"github"} url={"https://github.com/TheGreatShan"}
-                            bgColor={"black"} className={"mr-2"}/>
-                <Link href={"/about"} className={"text-white bg-black p-4 rounded-full"}>About</Link>
+        <div className={"border-solid border-[7px] border-white bg-[#e7e5e4] md:h-screen h-full"}>
+            <div
+                className="content-center bg-[#e7e5e4] h-screen md:h-full text-black font-libre border-solid border-[7px] p-10 border-black">
+                <h1 className="md:text-6xl text-3xl">Shansai</h1>
+                <h1 className="md:text-8xl text-4xl">Muraleetharan</h1>
+                <div className={"mt-4"}>
+                    <SocialIcon network={"linkedin"} url={"https://www.linkedin.com/in/shansai-muraleetharan"}
+                                bgColor={"black"} className={"mr-2"}/>
+                    <SocialIcon network={"github"} url={"https://github.com/TheGreatShan"}
+                                bgColor={"black"} className={"mr-2"}/>
+                    <Link href={"/about"} className={"text-white bg-black p-4 rounded-full"}>About</Link>
+                </div>
             </div>
         </div>
+
     );
 }
