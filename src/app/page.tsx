a808e79d1ebbb2dd8c58204014446634c2099c1a
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 14:07:27 +0200

    name is in middle of the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
     return (
-        <div className="bg-[#e7e5e4] h-screen text-black border-solid border-[7px] p-10 border-black">
+        <div className="bg-[#e7e5e4] h-screen text-black border-solid border-[7px] p-10 border-black content-center">
             <div className="font-libre">
-                <h1 className="text-2xl">Shansai</h1>
-                <h1 className="text-4xl">Muraleetharan</h1>
+                <h1 className="text-6xl">Shansai</h1>
+                <h1 className="text-8xl">Muraleetharan</h1>
             </div>
         </div>
     );
