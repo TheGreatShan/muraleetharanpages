3e3b8762bc76dd741772a39cac0d3fa95ce0b78e
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 11:08:35 +0200

    border makes everything more beautiful

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="bg-[#e7e5e4] h-screen text-black">
+        <div className="bg-[#e7e5e4] h-screen text-black border-solid border-[7px] p-10 border-black">
             <h1>Shansai</h1>
             <h1>Muraleetharan</h1>
         </div>
