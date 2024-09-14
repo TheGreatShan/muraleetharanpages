c74d5cdc7c6277c420c143a6487415edec9cb919
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 11:17:34 +0200

    Home page contains names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
     return (
         <div className="bg-[#e7e5e4] h-screen text-black border-solid border-[7px] p-10 border-black">
             <div className="font-libre">
-                <h1>Shansai</h1>
-                <h1>Muraleetharan</h1>
+                <h1 className="text-2xl">Shansai</h1>
+                <h1 className="text-4xl">Muraleetharan</h1>
             </div>
         </div>
     );
