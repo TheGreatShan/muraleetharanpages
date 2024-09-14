eb255c867b08826710afd431b7cfd05dcb4d811f
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 18:24:43 +0200

    background is over the full height of the page

--- src/app/components/design.tsx
+++ src/app/components/design.tsx
@@ -1,9 +1,10 @@
 import Nav from "muralee/app/components/Nav";
+import React from "react";
 
 export default function Design({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <>
-            <div className={"border-solid border-[7px] border-black bg-[#e7e5e4] text-black font-libre h-screen"}>
+            <div className={"border-solid border-[7px] border-black bg-[#e7e5e4] text-black font-libre h-full"}>
                 <Nav/>
                 <div className={"p-10"}>
                     {children}
