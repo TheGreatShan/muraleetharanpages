01ebc9f164c862bece7c3f011085b43e7accc0c6
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 14:41:31 +0200

    root page has manual design without <Design> component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,17 @@ import Design from "muralee/app/components/design";
 
 export default function Home() {
     return (
-        <Design>
-            <div className="content-center h-screen">
-                <h1 className="text-6xl">Shansai</h1>
-                <h1 className="text-8xl">Muraleetharan</h1>
-                <div className={"mt-4"}>
-                    <SocialIcon network={"linkedin"} url={"https://www.linkedin.com/in/shansai-muraleetharan"}
-                                bgColor={"black"} className={"mr-2"}/>
-                    <SocialIcon network={"github"} url={"https://github.com/TheGreatShan"}
-                                bgColor={"black"}/>
-                </div>
+
+        <div
+            className="content-center bg-[#e7e5e4] h-screen text-black font-libre border-solid border-[7px] p-10 border-black">
+            <h1 className="text-6xl">Shansai</h1>
+            <h1 className="text-8xl">Muraleetharan</h1>
+            <div className={"mt-4"}>
+                <SocialIcon network={"linkedin"} url={"https://www.linkedin.com/in/shansai-muraleetharan"}
+                            bgColor={"black"} className={"mr-2"}/>
+                <SocialIcon network={"github"} url={"https://github.com/TheGreatShan"}
+                            bgColor={"black"}/>
             </div>
-        </Design>
+        </div>
     );
 }
