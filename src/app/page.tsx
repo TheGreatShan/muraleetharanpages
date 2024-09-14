d29b721d02164d8bae9bee229c6d3f8e6aa453e0
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 18:20:11 +0200

    Mobile design for home page was created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,14 @@ export default function Home() {
 
         <div
             className="content-center bg-[#e7e5e4] h-screen text-black font-libre border-solid border-[7px] p-10 border-black">
-            <h1 className="text-6xl">Shansai</h1>
-            <h1 className="text-8xl">Muraleetharan</h1>
+            <h1 className="md:text-6xl text-3xl">Shansai</h1>
+            <h1 className="md:text-8xl text-4xl">Muraleetharan</h1>
             <div className={"mt-4"}>
                 <SocialIcon network={"linkedin"} url={"https://www.linkedin.com/in/shansai-muraleetharan"}
                             bgColor={"black"} className={"mr-2"}/>
                 <SocialIcon network={"github"} url={"https://github.com/TheGreatShan"}
                             bgColor={"black"} className={"mr-2"}/>
                 <Link href={"/about"} className={"text-white bg-black p-4 rounded-full"}>About</Link>
-
             </div>
         </div>
     );
