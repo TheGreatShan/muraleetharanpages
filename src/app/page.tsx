1d476a9c703b7d2cf096eb7ee30b3161f3229677
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-14 11:15:56 +0200

    libre baskerville is used as font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
     return (
         <div className="bg-[#e7e5e4] h-screen text-black border-solid border-[7px] p-10 border-black">
-            <h1>Shansai</h1>
-            <h1>Muraleetharan</h1>
+            <div className="font-libre">
+                <h1>Shansai</h1>
+                <h1>Muraleetharan</h1>
+            </div>
         </div>
     );
 }
