5677463488a6bfdd0d23a2f443e4d64ecde54ec5
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-15 21:19:06 +0200

    adding career

---
+++ src/app/career/page.tsx
@@ -0,0 +1,15 @@
+import Design from "muralee/app/components/design";
+import Header from "muralee/app/components/header";
+
+export default function Career(){
+    return (
+        <Design>
+            <div className={"mt-5"}>
+                <Header title={"Career"}/>
+                <p className={"mt-4 mx-[50px]"}>
+                    
+                </p>
+            </div>
+        </Design>
+    )
+}
