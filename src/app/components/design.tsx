a8ef5bbd6006132cf52bb4bd5617662da6850efd
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-15 08:39:16 +0200

    background is stretched till the end of the component

--- src/app/components/design.tsx
+++ src/app/components/design.tsx
@@ -4,9 +4,9 @@ import React from "react";
 export default function Design({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <>
-            <div className={"border-solid border-[7px] border-white bg-[#e7e5e4] md:h-screen h-full"}>
+            <div className={"border-solid border-[7px] border-white bg-[#e7e5e4] min-h-screen h-full flex flex-grow"}>
                 <div
-                    className={"border-solid border-[7px] border-black bg-[#e7e5e4] text-black font-libre h-full h-screen"}>
+                    className={"border-solid border-[7px] border-black bg-[#e7e5e4] text-black font-libre min-h-screen h-full"}>
                     <Nav/>
                     <div className={"md:p-10"}>
                         {children}
