79c4d2a29db8cd320179362fcca8e78600acb224
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-09-17 14:17:01 +0200

    page is mobile friendly

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -7,20 +7,27 @@ export default function Career() {
         <Design>
             <div className={"mt-5"}>
                 <Header title={"Career"}/>
-                <div className={"mt-4 mx-[50px]"}>
-                    <span className={"flex"}>
-                        <Image
-                            src={"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/KPMG_logo.svg/2560px-KPMG_logo.svg.png"}
-                            alt={"KPMG logo"}
-                            width={110}
-                            height={100}
-                        />
-                        <div className={"ml-5"}>
-                            <p className={"font-bold"}> | KPMG Switzerland | Apprentice Software Engineer</p>
-                            <p>| August 2021 - now | Hybrid</p>
+                <div className={"mt-4 mx-[50px] md:text-[12pt] text-[9pt]"}>
+                    <div className={"md:flex"}>
+                        <div className={"flex justify-center"}>
+                            <Image
+                                src={"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/KPMG_logo.svg/2560px-KPMG_logo.svg.png"}
+                                alt={"KPMG logo"}
+                                width={110}
+                                height={100}
+                            />
                         </div>
-                    </span>
-                    <div className={"mt-5"}>
+
+                        <div
+                            className={"md:ml-5 md:mt-0 mt-5 font-bold md:text-left text-center"}>
+                            <p className={"md:inline"}>KPMG Switzerland <span
+                                className={"md:inline md:visible invisible"}> | </span>
+                            </p>
+                            <p className={"md:inline"}>Apprentice Software Engineer</p>
+                            <p>August 2021 - now | Hybrid</p>
+                        </div>
+                    </div>
+                    <div className={"mx-5 mt-5"}>
                         <ul className={"list-disc"}>
                             <li>Working on a project which tracks the grade of KPMGs apprentices</li>
                             <li>Working on middleware which improves System interoperability</li>
