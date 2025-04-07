38e407b70a56884322759024a309ed8295fc1786
Shansai Muraleetharan <shansai@muraleetharan.ch>
2025-04-07 13:08:00 +0200

    Font sans is mainly used instead of libre

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -7,10 +7,10 @@ export default function Nav() {
 
 
     return (
-        <div className={"bg-[#e7e5e4] text-black font-libre p-10 backdrop-blur-sm select-none"}>
+        <div className={"bg-[#e7e5e4] text-black font-sans p-10 backdrop-blur-sm select-none"}>
             <div className={"fixed left-0 md:ml-[100px] ml-[50px] md:visible invisible"}>
-                <h1 className={"md:text-2xl text-sm"}>Shansai</h1>
-                <h1 className={"md:text-3xl text-sm"}>Muraleetharan</h1>
+                <h1 className={"md:text-2xl text-sm font-serif"}>Shansai</h1>
+                <h1 className={"md:text-3xl text-sm font-serif"}>Muraleetharan</h1>
             </div>
             <div className={"fixed right-0 mx-10"}>
                 <Link href={"/"} title={"Home"} className={"mr-10"}>Home</Link>
