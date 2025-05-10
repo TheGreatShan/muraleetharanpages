65c7bd72be8a615023bfed7320115ec94ae2deb7
Shansai Muraleetharan <shansai@muraleetharan.ch>
2025-05-10 18:48:51 +0200

    blueish border also in design

--- src/app/components/design.tsx
+++ src/app/components/design.tsx
@@ -18,9 +18,9 @@ export default function Design({children}: Readonly<{ children: React.ReactNode;
     return (
         <>
             <div
-                className={"border-solid border-[7px] border-white bg-primary-content min-h-screen w-screen h-full flex grow"}>
+                className={"border-solid border-[7px] border-info-content bg-primary-content min-h-screen w-screen h-full flex grow"}>
                 <div
-                    className={"border-solid border-[7px] border-black bg-primary-content text-black font-libre w-screen min-h-full"}>
+                    className={"bg-primary-content text-black font-libre w-screen min-h-full"}>
                     <Nav/>
                     <div className={"md:p-10 font-sans"}>
                         {children}
