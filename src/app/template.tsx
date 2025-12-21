cca97afdbef37373bbf02021086fd45917de1aea
Shansai Muraleetharan <shansai@muraleetharan.ch>
2025-12-21 08:29:38 +0100

    snoflakes have been added

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Snowfall from "react-snowfall";
+import Snowflake from "react-snowfall/lib/Snowflake";
 
 export default function Template({
   children,
@@ -13,6 +15,7 @@ export default function Template({
       animate={{ y: 0, opacity: 1 }}
       transition={{ type: "easeInOut", stiffness: 50 }}
     >
+      <Snowfall />
       {children}
     </motion.div>
   );
