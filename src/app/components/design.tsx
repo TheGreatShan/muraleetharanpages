7288d4d0c57d64439f1738a376b71fab1464dfbc
Shansai Muraleetharan <shansai@muraleetharan.ch>
2025-12-22 12:08:42 +0100

    fix: removed current time on subpages

--- src/app/components/design.tsx
+++ src/app/components/design.tsx
@@ -6,15 +6,6 @@ import { useState, useEffect } from "react";
 
 
 export default function Design({children}: Readonly<{ children: React.ReactNode; }>) {
-    const [ctime, setTime] = useState<string>("");
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(new Date().toLocaleTimeString("en-US", { hour12: false }));
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
     return (
         <>
             <div
@@ -25,16 +16,9 @@ export default function Design({children}: Readonly<{ children: React.ReactNode;
                     <div className={"md:p-10 font-sans"}>
                         {children}
                     </div>
-                        <div className="hidden md:block fixed bottom-0 left-0 ml-10 mb-10 text-xs select-none">
-                            <h1 className="font-bold">&copy; {new Date().getFullYear() > 2025 && `2025 -`} {new Date().getFullYear()} Shansai Muraleetharan</h1>
-                        </div>
-                        <div className={"fixed bottom-0 right-0 text-2xl font-bold mr-10 mb-10 select-none"}>
-                        {
-                            ctime == "" 
-                            ? <span className="loading loading-dots loading-xs"></span>
-                            : <h1>{ctime}</h1>
-                        }
-                        </div>
+                    <div className="hidden md:block fixed bottom-0 left-0 ml-10 mb-10 text-xs select-none">
+                        <h1 className="font-bold">&copy; {new Date().getFullYear() > 2025 && `2025 -`} {new Date().getFullYear()} Shansai Muraleetharan</h1>
+                    </div>
                 </div>
             </div>
         </>
